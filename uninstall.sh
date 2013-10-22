74a17f1ac9d55f60f82d5b5eb5942f8b300c1c9c
Nitkalya Wiriyanuparb <dekpient@gmail.com>
2013-10-23 02:07:18 +1300

    Installation, deb package generation, and uninstallation

--- uninstall.sh
+++ uninstall.sh
@@ -10,14 +10,14 @@
 ###################################################################
 
 echo "######################################################################"
-echo "Killing processes"
+echo "Killing all UpStage processes"
 #find the process id's associated with upstage-server
 out=`ps ux | awk '/upstage-server/ && !/awk/ {print $2}'`
 #loop through the processes and kill each one.
 echo "Processes found."
 for pid in $out
 do
-    	echo "Stopping $pid"
+    echo "Stopping $pid"
 	kill $pid
 	echo "$pid stopped."
 done
@@ -42,6 +42,15 @@ echo "Removing /usr/local/share/upstage/"
 rm -r /usr/local/share/upstage/
 echo "Removing /etc/cron.weekly/upstage-backup.sh"
 rm /etc/cron.weekly/upstage-backup.sh
+echo "Removing symbolic links in /usr/local/bin"
+rm /usr/local/bin/chownme.sh
+rm /usr/local/bin/img2swf.py
+rm /usr/local/bin/speaker.py
+rm /usr/local/bin/upstage-admin
+rm /usr/local/bin/upstage-admin.conf
+rm /usr/local/bin/upstage-backup
+rm /usr/local/bin/upstage-server
+
 
 echo ""
 echo "Thank you for using UpStage!"
